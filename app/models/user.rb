6a41e44224f3acacbdbdfe579dc298af3ed5ebfc
jjkazm <jjkazm@gmail.com>
2019-02-03 00:46:57 +0100

    seed database

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :exercises
+  has_many :exercises, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+
+
+emails = ["kuba@wp.pl", "agatka@wp.pl", "jurek@wp.pl"]
+
+emails.map { |address| User.create(email: address, password: "haslo123" )}
+10.times { User.create(email: Faker::Internet.unique.email, password: "haslo123" )}
+
+workouts = ["Jogging", "Swimming", "Weightlifitng", "Karate"]
+
+User.all.each do |person|
+  ((Date.today - 7)..Date.today).to_a.each do |day|
+    Exercise.create(user_id: person.id, duration_in_min: rand(100), workout: workouts[rand(4)], workout_date: day)
+  end
+end
