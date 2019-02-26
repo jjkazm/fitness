d9348ada89260b9b921d6f165bd412666e5dff6c
jjkazm <jjkazm@gmail.com>
2019-02-26 21:20:18 +0100

    Test user first and last name validations

--- spec/features/sign_up_user_spec.rb
+++ spec/features/sign_up_user_spec.rb
@@ -21,4 +21,29 @@ RSpec.feature 'Sign up user' do
       expect(page).to have_link("Signed in as kuba@wp.pl")
       expect(page).to have_content("Kuba Doe")
   end
+  scenario 'with invalid credentials' do
+      visit "/"
+
+      click_link 'Signup'
+
+      fill_in "First name", with: ""
+      fill_in "Last name", with: ""
+      fill_in "Email", with: "kuba@wp.pl"
+      fill_in "Password", with: "haslo123"
+      fill_in "Password confirmation", with: "haslo123"
+
+      click_button "Signup"
+
+
+      expect(page).not_to have_content "First name can't be blank"
+      expect(page).not_to have_content "Last name can't be blank"
+      expect(page).not_to have_content "Welcome! You have signed up successfully."
+      expect(page).to have_link("Signup")
+      expect(page).to have_link("Log in")
+      expect(page).not_to have_link("Sign out")
+      expect(page).not_to have_link("Signed in as kuba@wp.pl")
+      expect(page).to have_content("Kuba Doe")
+  end
+
+
 end
